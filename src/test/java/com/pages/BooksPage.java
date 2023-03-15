f6c8896989f15f664b7552aaea1d8914ea47cf9d
nezih <m.nezihkaya@gmail.com>
2023-03-14 20:56:54 -0400

    BooksPage

---
+++ src/test/java/com/pages/BooksPage.java
@@ -0,0 +1,41 @@
+package com.pages;
+
+import com.Utils.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class BooksPage {
+    public BooksPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//select[@name='tbl_books_length']")
+    public WebElement showRecordsDropdown;
+
+    @FindBy (xpath = "//th[text()='Borrowed By']")
+    public WebElement filterBorrowedBy;
+
+    @FindBy (xpath = "//td[7]/text()/..")
+    public List<WebElement> borrowedStatus;
+
+    @FindBy (xpath = "//td[7]/text()/../preceding-sibling::td[2]")
+    public List<WebElement> genreStatus;
+
+    @FindBy (xpath = "//input[@type='search']")
+    public WebElement searchBooksInput;
+
+    @FindBy (xpath = "//td[3]")
+    public WebElement bookName;
+
+    @FindBy (xpath = "//td[4]")
+    public WebElement bookAuthor;
+
+    @FindBy (xpath = "//td[6]")
+    public WebElement bookYear;
+
+    @FindBy (id = "book_categories")
+    public WebElement bookCategories;
+}
