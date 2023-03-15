1f043a013352e89b01f451ca2e4da2f038e0f8ef
nezih <m.nezihkaya@gmail.com>
2023-03-14 21:00:13 -0400

    main sql

---
+++ Queries/main.sql
@@ -0,0 +1,28 @@
+select * from book_categories;
+select * from book_borrow;
+select * from books;
+
+
+select book_categories.name, count(*) TotalCount from books
+inner join book_borrow on books.id = book_borrow.book_id
+inner join book_categories on books.book_category_id = book_categories.id
+group by book_categories.name
+order by TotalCount desc;
+
+select name from book_categories
+where name = (
+    select book_categories.name from books
+    inner join book_borrow on books.id = book_borrow.book_id
+    inner join book_categories on books.book_category_id = book_categories.id
+    group by book_categories.name
+    order by count(*) desc
+    limit 1
+    );
+
+select full_name,count(*) from users u inner join book_borrow bb on u.id = bb.user_id
+group by full_name
+order by 2 desc ;
+
+select name, author, year from books where name='Chordeiles minor';
+
+select name from book_categories;
---
+++ src/test/resources/features/borrowed_books.feature
@@ -0,0 +1,8 @@
+Feature: As a librarian, I want to know the amount of borrowed books
+
+  Background:
+    Given user log in as a librarian
+
+  Scenario: verify the amount of borrowed books
+    When user take borrowed books number
+    Then borrowed books number information must match with DB
