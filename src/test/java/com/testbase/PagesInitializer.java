38769e58868b2b9d561f51584dc419faf426dadd
nezih <m.nezihkaya@gmail.com>
2023-03-14 20:57:53 -0400

    PagesInit added

---
+++ src/test/java/com/testbase/PagesInitializer.java
@@ -0,0 +1,12 @@
+package com.testbase;
+
+import com.pages.BooksPage;
+import com.pages.DashboardPage;
+import com.pages.LoginPage;
+
+public class PagesInitializer {
+
+    public LoginPage loginPage = new LoginPage();
+    public DashboardPage dashboardPage = new DashboardPage();
+    public BooksPage booksPage = new BooksPage();
+}
